81328305a2dfffbdc33d3dbd06a8d9b5d216f396
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2023-01-30 23:36:58 +0300

    testing bot in local.

--- web/telegram-bot.js
+++ web/telegram-bot.js
@@ -60,10 +60,14 @@ bot.on('text', async (msg) => {
     return msg.reply.text(_message)
 });
 
+bot.on('photo', (msg) => { // what do we want to do on edit?, reprocess the message/command? or do nothing for now?
+    console.log('msg', msg);
+});
+
 bot.on('edit', (msg) => { // what do we want to do on edit?, reprocess the message/command? or do nothing for now?
     return msg.reply.text('I saw it! You edited a message!', { asReply: true });
 });
 
-if (process.env.NODE_ENV && process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
     bot.start(); // only run in prod, there can only be one instance!
 }
